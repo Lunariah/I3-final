b8c4eaf8888ec1ddebfd52164c2c7f43bf621e01
Luna <luna.lorenz@protonmail.com>
2020-08-13 23:11:41 +0200

    charge jauge

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Transform trans;
+    public Transform chargeJauge;
+
+    public float movementSpeed = 3;
+    public float chargeSpeed = 1;
+
+    private float charge = 0; // Between 0 and 1
+    private float minCharge = 0.2f;
+
+
+    private void Start()
+    {
+        trans = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        // Movement
+        trans.position += new Vector3(movementSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0);
+
+        // Charge
+        if (Input.GetButton("Fire1"))
+        {
+            charge = Mathf.Min(charge + chargeSpeed * Time.deltaTime, 1);
+
+        }
+        else
+        {
+            if (Input.GetButtonUp("Fire1") && charge >= minCharge)
+            {
+                // Fire
+                Debug.Log("Pew!");
+            }
+            charge = Mathf.Max(charge - 3 * Time.deltaTime, 0);
+        }
+        chargeJauge.localScale = new Vector3(charge, chargeJauge.localScale.y, chargeJauge.localScale.z);
+    }
+}
